8a6a7280412ce52b3024adcd1e965e4131247420
Artur Gaster <gasterartur@googlemail.com>
2024-10-10 13:38:16 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+import React from 'react';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
